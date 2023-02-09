2f9e4de1f0446f64041c2b3718fc0aaa2210913d
yxswy <18267094443@163.com>
2023-02-09 15:36:41 +0800

    添加可配置

--- src/extension.ts
+++ src/extension.ts
@@ -1,48 +1,72 @@
 import { existsSync } from "fs";
 import { join } from "path";
 import {
-	workspace,
-	window,
 	ExtensionContext,
 	TerminalOptions,
-	StatusBarItem,
 	StatusBarAlignment,
+	workspace,
+	window,
 	commands,
 } from "vscode";
 
-let myStatusBarItem: StatusBarItem;
 let openFolder: string = "dist";
 
 export function activate({ subscriptions }: ExtensionContext) {
-	const { workspaceFolders } = workspace;
-	if (!workspaceFolders) {
-		console.log("Please open the folder and try again.");
-		return;
-	}
-
-	const myCommandId = "sample.showSelectionCount";
-	subscriptions.push(
-		commands.registerCommand(myCommandId, () => {
-			const rootFolderPath = workspaceFolders[0].uri.fsPath;
-			const distPath = join(rootFolderPath, openFolder);
-			// 判断dist文件夹是否存在
-			if (existsSync(distPath)) {
-				const options: TerminalOptions = {
-					shellPath:
-						"C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe",
-					name: "powershell",
-				};
-				const terminal = window.createTerminal(options);
-				terminal.sendText(`explorer ${distPath}`);
-				terminal.show();
-			}
-		}),
+	let myStatusBarItem = window.createStatusBarItem(
+		StatusBarAlignment.Right,
+		100,
 	);
 
-	myStatusBarItem = window.createStatusBarItem(StatusBarAlignment.Right, 100);
-	myStatusBarItem.show();
-	myStatusBarItem.command = myCommandId;
-	myStatusBarItem.text = "open dist";
-	myStatusBarItem.color = "white";
-	myStatusBarItem.tooltip = "Parinfer is in mode";
+	const myCommandId = "sample.showSelectionCount";
+
+	const fn = commands.registerCommand(myCommandId, () => {
+		const { workspaceFolders } = workspace;
+		if (!workspaceFolders) {
+			console.log("Please open the folder and try again.");
+			return;
+		}
+
+		const rootFolderPath = workspaceFolders[0].uri.fsPath;
+		const distPath = join(rootFolderPath, openFolder);
+		// 判断dist文件夹是否存在
+		if (existsSync(distPath)) {
+			const options: TerminalOptions = {
+				shellPath:
+					"C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe",
+				name: "powershell",
+			};
+			const terminal = window.createTerminal(options);
+			terminal.sendText(`explorer ${distPath}`);
+			terminal.show();
+		}
+	});
+
+	function core() {
+		const workbenchConfig = workspace.getConfiguration("mfts");
+		const enabled: boolean = workbenchConfig.get("enabled") ?? true;
+		openFolder = workbenchConfig.get("distPath") ?? "dist";
+
+		if (!enabled) {
+			const index = subscriptions.findIndex((el) => el === fn);
+			if (index !== -1) {
+				subscriptions.splice(index, 1);
+			}
+			myStatusBarItem.hide();
+			return;
+		}
+
+		subscriptions.push(fn);
+
+		myStatusBarItem.show();
+		myStatusBarItem.command = myCommandId;
+		myStatusBarItem.text = "open dist";
+		myStatusBarItem.color = "white";
+		myStatusBarItem.tooltip = "Parinfer is in mode";
+	}
+
+	workspace.onDidChangeConfiguration((e) => {
+		core();
+	});
+
+	core();
 }
